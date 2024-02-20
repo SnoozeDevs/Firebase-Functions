d35699c8fbd2de91ca4f62e35eb3b5dfdc244b77
Connor Rendoth <connorrendoth11@gmail.com>
2024-02-20 21:43:21 +1100

    updates to functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,11 +9,29 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import axios from "axios";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
+export const getTeams = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  axios.get("https://api.squiggle.com.au/?q=teams",{
+    headers:{
+      "User-Agent":"easytippingdev@gmail.com"
+    }
+  }).then((res)=>{
+    response.send(res.data);
+  })
+});
+
+export const getGames2022 = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  axios.get("https://api.squiggle.com.au/?q=games&year=2022",{
+    headers:{
+      "User-Agent":"easytippingdev@gmail.com"
+    }
+  }).then((res)=>{
+    response.send(JSON.stringify(res.data));
+  })
 });
