07be6f9cac74ccff58fdd5bdedd1b3d056cce766
Connor Rendoth <connorrendoth11@gmail.com>
2024-02-22 08:47:00 +1100

    Clean Up

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import {onRequest} from "firebase-functions/v2/https";
 import axios from "axios";
 const {initializeApp} = require("firebase-admin/app");
@@ -14,6 +5,8 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 initializeApp();
 
+//import {onCall} from "firebase-functions/v2/https";
+//import {onDocumentWritten} from "firebase-functions/v2/firestore";
 //const {getFirestore} = require("firebase-admin/firestore");
 //const db = getFirestore();
 
